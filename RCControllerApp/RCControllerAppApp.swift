5502aea065159dd4ebffdb8cb9343b2e1e49b934
Lewin Sieben <lewin.sieben@check24.de>
2024-05-03 11:11:45 +0200

    Initial Commit

---
+++ RCControllerApp/RCControllerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCControllerAppApp.swift
+//  RCControllerApp
+//
+//  Created by Lewin Sieben on 03.05.24.
+//
+
+import SwiftUI
+
+@main
+struct RCControllerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
