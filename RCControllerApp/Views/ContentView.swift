f6359d60642c52ce9f9eb0ebd27a1f980d7b5e53
SiebenDigital <lewin.7777777@gmail.com>
2024-05-03 12:50:39 +0200

    UI Changes

---
+++ RCControllerApp/Views/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  RCControllerApp
+//
+//  Created by Lewin Sieben on 03.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var direction = Float(1.0)
+    @State var speed = Float(1.0)
+    
+    var body: some View {
+        NavigationStack {
+            HStack {
+                Text("Connection Status")
+                    .font(.title3)
+                    .fontDesign(.rounded)
+                
+                RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
+                    .frame(width: 100, height: 30)
+                    .foregroundColor(Color.lightGreen)
+                
+                Spacer()
+            }
+            .padding()
+            
+            
+            VStack
+            
+            VStack(alignment: .leading) {
+                Text("\(direction, specifier: "%.2f")")
+                WheelControl(value: $direction, range: -45.0...45.0, orientation: .horizontal)
+                    .frame(maxHeight: 80)
+            }
+            .padding()
+            
+            VStack(alignment: .leading, content: {
+                WheelControl(value: $speed, range: -100.0...100.0)
+                    .frame(maxWidth: 80, maxHeight: 150)
+            })
+        
+            
+          
+            .navigationTitle("RC-Contoller")
+                
+        }
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
